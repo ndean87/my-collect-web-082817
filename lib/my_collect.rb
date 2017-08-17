816502c156e29551b26751d75d3c714f8e7828ce
Natalie Dean <ndean87@gmail.com>
2017-08-17 20:45:53 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(collection)
+  if block_given?
+    new_collection_array = []
+    collection.each do |element|
+      new_collection_array.push yield element
+    end
+    new_collection_array
+  else
+    puts "No block was given!"
+  end
+end
